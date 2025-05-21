4a96026b0c874543b7507030b8bb8afc44b780bf
jelita <jelita.com>
2025-05-21 19:13:42 +0700

    menyelesaikan halaman produk list

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/produk', function () {
+    return view('produk');
+})->name('produk');
