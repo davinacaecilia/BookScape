c05edff49629dd123cce541ac04b821da342c56e
Dhaifina Raiqa Zahira <Raiqazahira0202@gmail.com>
2025-05-22 13:32:42 +0700

    Harus Sign In sebelum Dashboard

--- routes/web.php
+++ routes/web.php
@@ -20,37 +20,26 @@ Route::get('/register', [RegisterController::class, 'showRegisterForm'])->name('
 Route::post('/register', [RegisterController::class, 'register']);
 
 Route::middleware('auth')->group(function () {
+    // dashboard
     Route::get('/dashboard', [UserController::class, 'showDashboard'])->name('dashboard');
     Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-
     Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
+
+    // admin views
+    Route::get('/message', fn() => view('admin.message'));
+    Route::get('/orders', fn() => view('admin.orders'));
+
+    // product management
+    Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+    Route::post('/product/store', [ProductController::class, 'store'])->name('product.store');
+    Route::get('/product-management', [ProductController::class, 'index'])->name('product.management');
+    Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name('product.edit');
+    Route::put('/product/{id}/update', [ProductController::class, 'update'])->name('product.update');
+    Route::delete('/product/{id}/delete', [ProductController::class, 'destroy'])->name('product.destroy');
+
+    // user management
+    Route::get('/user-management', [AdminController::class, 'listUsers']);
 });
-
-
-//Route untuk dashboard admin (punya anggun)
-Route::get('/message', function () {
-    return view('admin.message');
-});
-Route::get('/orders', function () {
-    return view('admin.orders');
-});
-
-// Ini Untuk Folder Product
-Route::get('/product/create', [ProductController::class, 'create'])->name('product.create'); // Add Product
-Route::post('/product/store', [ProductController::class, 'store'])->name('product.store');
-Route::get('/product-management', [ProductController::class, 'index'])->name('product.management');
-
-Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name('product.edit'); // Edit Product
-Route::put('/product/{id}/update', [ProductController::class, 'update'])->name('product.update');
-Route::put('/product/{id}', [ProductController::class, 'update'])->name('product.update'); // buat update data
-
-// Button Delete
-Route::delete('/product/{id}/delete', [ProductController::class, 'destroy'])->name('product.destroy');
-    // Tempat untuk delete data dari database nanti
-    // return redirect()->route('product.management')->with('success', 'Product deleted!');
-
-// User Mnagement
-Route::get('/user-management', [AdminController::class, 'listUsers']);
 
 //Route untuk list produk di hlmn user (punya jeilta)
 Route::get('/produk', function () {
