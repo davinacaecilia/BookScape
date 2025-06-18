85b2c6ab185bbd8f854e3f74b033db8b87fb3e31
putrisahara <putrisaharatampubolon.com>
2025-06-18 15:15:24 +0700

    halaman home cuma ada home.blade , home.css sm web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LandingController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\Homecontroller;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('/', [LandingController::class, 'index'])->name('welcome');
+Route::post('/login', [LandingController::class, 'login'])->name('login');
+// Route::post('/logout', [LandingController::class, 'logout'])->name('logout');
+Route::get('/register', [LandingController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [LandingController::class, 'register']);
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+Route::get('/home', [HomeController::class,'home'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::post('/logout', [ProfileController::class, 'logout'])->name('logout');
 });
